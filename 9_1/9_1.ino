eb7028cd8dca8c935632d9969b8a232ee6cfee44
tbenra <52466686+tbenra@users.noreply.github.com>
2019-07-02 20:53:18 +0200

    Add files via upload

---
+++ 9_1/9_1.ino
@@ -0,0 +1,24 @@
+int LED_Pin = 10;
+int LED_status= LOW;
+const long zeit = 1000;
+unsigned long zeitdamals = 0;
+
+void setup() {
+
+  pinMode(LED_Pin, OUTPUT);
+}
+
+void loop() {
+  unsigned long zeitjetzt = millis();
+  if(zeitjetzt - zeitdamals >= zeit){
+  zeitdamals=zeitjetzt;
+  if(LED_status==LOW){
+    LED_status=HIGH;
+    } 
+    else { 
+      LED_status==LOW;
+      }
+      digitalWrite(LED_Pin, LED_status);
+  }
+                      
+}
